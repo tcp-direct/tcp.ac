256884a0f65d6af4c079c91375549b022220c16e
kayos@tcp.direct <kayos@tcp.direct>
2020-12-16 00:19:23 -0800

    thats a string idiot

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func imgPost(c *gin.Context) {
 	Hashr.Write(Scrubbed)
 	hash := Hashr.Sum(nil)
 
-	fmt.Printf("[imgPost][%d] MD5: %x\n", uid, hash)
+	fmt.Printf("[imgPost][%s] MD5: %x\n", uid, hash)
 
 	fmt.Println("[imgPost][" + uid + "] Checking for duplicate's in database")
 
