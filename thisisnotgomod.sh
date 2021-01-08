9cf0df22b863089d7fdc8e58c9a4a69267438cc6
kayos@tcp.direct <kayos@tcp.direct>
2021-01-07 20:37:40 -0800

    yung RETARD

---
+++ thisisnotgomod.sh
@@ -0,0 +1,5 @@
+go get github.com/asaskevich/govalidator
+go get github.com/scottleedavis/go-exif-remove
+go get golang.org/x/crypto/blake2b
+go get github.com/twharmon/gouid
+go get github.com/gin-gonic/gin
